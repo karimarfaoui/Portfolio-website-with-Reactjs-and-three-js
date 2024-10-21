813e92d2b40c56c168cd66da515400e8d2107207
karim <arfaouikarim77@gmail.com>
2024-10-21 10:25:29 +0100

    portfilio complited

---
+++ src/pages/About.jsx
@@ -0,0 +1,129 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+
+import { CTA } from "../components";
+import { experiences, skills } from "../constants";
+
+import "react-vertical-timeline-component/style.min.css";
+
+const About = () => {
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm{" "}
+        <span className='blue-gradient_text font-semibold drop-shadow'>
+          {" "}
+          Karim
+        </span>{" "}
+        👋
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+          Software Engineer based in Tunisie, specializing in technical
+          education through hands-on learning and building applications.
+        </p>
+      </div>
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {skills.map((skill) => (
+            <div className='block-container w-20 h-20' key={skill.name}>
+              <div className='btn-back rounded-xl' />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className='w-1/2 h-1/2 object-contain'
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='py-16'>
+        <h3 className='subhead-text'>Work Experience.</h3>
+        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+          <p>
+            I've worked with this companies, leveling up my skills and
+            teaming up with smart people. Here's the rundown:
+          </p>
+        </div>
+
+        <div className='mt-12 flex'>
+          <VerticalTimeline>
+            {experiences.map((experience, index) => (
+              <VerticalTimelineElement
+                key={experience.company_name}
+                date={experience.date}
+                iconStyle={{ background: experience.iconBg }}
+                icon={
+                  <div className='flex justify-center items-center w-full h-full'>
+                    <img
+                      src={experience.icon}
+                      alt={experience.company_name}
+                      className='w-[60%] h-[60%] object-contain'
+                    />
+                  </div>
+                }
+                contentStyle={{
+                  borderBottom: "8px",
+                  borderStyle: "solid",
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: "none",
+                }}
+              >
+                <div>
+                  <h3 className='text-black text-xl font-poppins font-semibold'>
+                    {experience.title}
+                  </h3>
+                  <p
+                    className='text-black-500 font-medium text-base'
+                    style={{ margin: 0 }}
+                  >
+                    {experience.company_name}
+                  </p>
+                </div>
+
+                <ul className='my-5 list-disc ml-5 space-y-2'>
+                  {experience.points.map((point, index) => (
+                    <li
+                      key={`experience-point-${index}`}
+                      className='text-black-500/50 font-normal pl-1 text-sm'
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+      <h2 className="text-3xl font-bold text-gray-800 mb-4">This is my CV</h2>
+
+<hr className='border-slate-200'/>
+
+<div className="flex justify-between my-4">
+<a href="/src/assets/cv/Karim_CV.pdf" download>
+<button className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+Download French CV  </button>
+</a>
+<a href="/src/assets/cv/karimCvEng.pdf" download>
+<button className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+Download CV in English  </button>
+</a>
+
+</div>
+      <hr className='border-slate-200' />
+
+      <CTA />
+    </section>
+  );
+};
+
+export default About;
